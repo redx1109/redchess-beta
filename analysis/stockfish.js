9f157cbbde91ed8ef0b678e211061b8624dc65b0
redx1109 <parmarindrajeet46@gmail.com>
2026-08-08 09:56:38 +0530

    testtt

--- analysis/stockfish.js
+++ analysis/stockfish.js
@@ -7,7 +7,7 @@ const SF_DEPTH = 15;
 
 function initStockfish() {
     try {
-        stockfish = new Worker("../engine/stockfish-18.js");
+        stockfish = new Worker("../engine/stockfish-18-lite.js");
         console.log("Stockfish worker created, waiting for engine...");
         stockfish.onmessage = e => {
             console.log("SF message:", e.data);
